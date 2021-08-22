4d4820914d2ad71868d4c549e2c22e7b09b59595
tYin666 <tie.yin@gmail.com>
2021-08-22 20:18:02 +0200

    add listInsert and findingtheprecentage.py

---
+++ FindingThePercentage.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    query_score = student_marks[query_name]
+    print("%.2f" %float(sum(query_score)/len(query_score)))
