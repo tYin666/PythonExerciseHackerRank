117d968a4b8afab7eb4d9e2535df4e2e6806e11b
tYin666 <tie.yin@gmail.com>
2021-08-24 20:43:13 +0200

    add the implementation of evalinput and exception

---
+++ Exception.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        a, b = input().split()
+        try:
+            print(int(a) // int(b))
+        except BaseException as err:
+            print("Error Code:", err)
---
+++ evalInput.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    #s = input()
+    #lis = s.split(' ')
+    #x = int(lis[0])
+    #k = int(lis[1])
+    x, k = map(int,input().split(" "))
+    print(eval(input()) == k) 
